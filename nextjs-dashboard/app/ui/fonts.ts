85f06da0867588b188ec730bb3842d093ccde177
malshqar <m12342al@gmail.com>
2024-09-22 12:58:09 +0300

    init app

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana } from "next/font/google";
+export const inter = Inter({
+     subsets: ["latin"],
+     display: "swap" ,
+     variable:'--font-inter'
+    });
+
+export const lusitana = Lusitana({
+    weight: ['400','700'], 
+    subsets: ["latin"], 
+    display: "swap" ,
+    variable:"--font-lusitana"
+});
